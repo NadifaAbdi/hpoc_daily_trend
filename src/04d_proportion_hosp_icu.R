673303c2c55342d5e2aa59f54466c82d7f6c7f06
Michael-Elten <77127921+Michael-Elten@users.noreply.github.com>
2021-04-01 08:40:07 -0400

    With new weekly figure

--- src/04d_proportion_hosp_icu.R
+++ src/04d_proportion_hosp_icu.R
@@ -143,7 +143,8 @@ plot_monthly<-function(type=""){
     group_by(earliestdate_month, agegroup10) %>%
     summarise(yes_outcome=sum(yes_outcome),
               no_outcome=sum(no_outcome),
-              total_outcome=sum(total_outcome)) %>%
+              total_outcome=sum(total_outcome),
+              .groups="drop_last") %>%
     mutate(proportion_outcome=yes_outcome/total_outcome,
            proportion_outcome=ifelse(is.na(proportion_outcome),0,proportion_outcome))
     
@@ -190,3 +191,65 @@ plot_monthly<-function(type=""){
 
 plot_monthly(type="hosp")
 plot_monthly(type="icu")
+
+plot_weekly<-function(type="", age_cat_filter=FALSE){
+  
+  data<-create_proportion_dataset(type=type) %>%
+    select(earliestdate, agegroup10, yes_outcome, no_outcome)%>%
+    mutate(total_outcome=yes_outcome+no_outcome,
+           earliestdate_week=lubridate::floor_date(earliestdate, unit="week")) %>%
+    filter(earliestdate>="2020-12-13")%>%
+    filter(if (age_cat_filter==TRUE) agegroup10 %in% c("20 to 29","30 to 39") else TRUE) %>%
+    group_by(earliestdate_week, agegroup10) %>%
+    summarise(yes_outcome=sum(yes_outcome),
+              no_outcome=sum(no_outcome),
+              total_outcome=sum(total_outcome),
+              .groups="drop_last") %>%
+    mutate(proportion_outcome=yes_outcome/total_outcome,
+           proportion_outcome=ifelse(is.na(proportion_outcome),0,proportion_outcome))
+  
+  
+  if(type=="hosp"){
+    event_name<-"hospitalization"
+  } else if (type=="icu"){
+    event_name<-"ICU"  
+  }
+  
+  
+  ggplot(data, aes(x=earliestdate_week, y=proportion_outcome, colour=agegroup10))+
+    geom_line(size=1.5)+
+    scale_y_continuous(paste0("Proportion of cases resulting in ",event_name),
+                       labels=label_percent()) +
+    scale_x_date(
+      "Date of illness onset",
+      breaks = ("month"),
+      labels = label_date("%d%b")) +
+    geom_rect(aes(xmin = max(data$earliestdate_week)-14,
+                  xmax = max(data$earliestdate_week),
+                  ymin = -Inf,
+                  ymax = Inf),
+              alpha = 0.01, fill = "grey", inherit.aes = FALSE) +
+    scale_color_tableau()+
+    guides(colour = guide_legend(override.aes = list(size=3), nrow=1))+
+    labs(caption = paste0("* Shaded area represents approximate lag in reporting")) +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.background = element_blank(),
+      axis.line = element_line(colour = "black"),
+      strip.background = element_blank(),
+      strip.text = element_text(hjust = 0, size = 26, face = "bold"),
+      legend.position = "bottom",
+      legend.title = element_blank(),
+      legend.key = element_blank(),
+      legend.text = element_text(size = 16),
+      legend.key.size = unit(3, "line"),
+      text = element_text(size = 20),
+      plot.caption = element_text(hjust = 0)
+    )
+}
+
+plot_weekly(type="hosp")
+plot_weekly(type="icu")
+plot_weekly(type="hosp", age_cat_filter = TRUE)
+plot_weekly(type="icu", age_cat_filter = TRUE)
